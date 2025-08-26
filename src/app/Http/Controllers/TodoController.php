191e2427f401541233c59bd9ff6fee988bbd1b17
Your Name <you@example.com>
2025-08-26 10:51:09 +0900

    DBに登録されているデータの取得

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -2,13 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+// 追加
+use App\Todo;
+
+// use Illuminate\Http\Request;　S8で書かれていなかったから
 
 class TodoController extends Controller
 {
     // <ここから>
     public function index()
     {
+// 追加
+        $todo = new Todo();//インスタンス化
+        $todos = $todo->all();//SQL文を組み立てず、todosテーブルから全てのレコードを取得することができる。
+        // dd($todos);//デバッグ（PHP Appの時のvar_dumpと同じ役割）。Illuminate\Database\Eloquent\Collectionクラスのインスタンス。
+
         return view('todo.index'); // 修正
     }
     // <ここまで>
