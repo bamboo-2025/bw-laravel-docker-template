a5c184a8f3de3669918779315bf4a7df6ccaa115
Your Name <you@example.com>
2025-08-27 10:31:18 +0900

    取得したデータを画面に渡す

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -17,7 +17,7 @@ class TodoController extends Controller
         $todos = $todo->all();//SQL文を組み立てず、todosテーブルから全てのレコードを取得することができる。
         // dd($todos);//デバッグ（PHP Appの時のvar_dumpと同じ役割）。Illuminate\Database\Eloquent\Collectionクラスのインスタンス。
 
-        return view('todo.index'); // 修正
+        return view('todo.index',['todos' => $todos]); // 修正。view関数は画面に表示したいbladeファイルを第一引数で指定し、第二引数に渡したいデータを連想配列の形で渡すことができる。
     }
     // <ここまで>
 }
